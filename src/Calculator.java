b8d4d52aadecab04c2b4bf65adfeb182238dd429
Tanay-Dwivedi <91426152+Tanay-Dwivedi@users.noreply.github.com>
2022-11-09 19:43:31 +0530

    added the button, frame and panel

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,11 +1,26 @@
 // building......
 
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Calculator implements ActionListener {
 
+    JFrame calcFrame;
+    JTextField calcTextField;
+    JButton [] calcNumberButtons = new JButton[10];
+    JButton [] calcFunctionButtons = new JButton[8];
+    // function buttons
+    JButton addition, subtraction, multiplication, division;
+    JButton decimals, equals, delete, allClear;
+    // panel to hold all the buttons
+    JPanel btnPanel;
+
     public static void main(String[] args) {
+
+        // creating instance of calculator class
+        Calculator calc = new Calculator();
+
     }
 
     // creating a constructor
